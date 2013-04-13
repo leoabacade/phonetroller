67b84a1b5ffa270d8472453fbae1968650479b0a
leoabacade <leoabacade@gmail.com>
2013-04-13 09:41:33 -0500

    add google+ page

--- webroot/index.php
+++ webroot/index.php
@@ -27,7 +27,7 @@ a:hover {
 <br />
 <img src="logo.jpg" id="logo" title="Phonetroller Logo" />
 <br /><br />
-<a href="http://www.twitter.com/phonetroller" target="_blank">twitter</a> | <a href="https://www.facebook.com/pages/Phonetroller/506683629367369"target="_blank" >facebook</a>
+<a href="http://www.twitter.com/phonetroller" target="_blank">twitter</a> | <a href="https://www.facebook.com/pages/Phonetroller/506683629367369"target="_blank" >facebook</a> | <a href="https://plus.google.com/b/115643990149833730252/115643990149833730252/posts"target="_blank" >google+</a> 
 
 
 <br /><br />
