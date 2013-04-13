1c58efe633566c897d3638024b4a9ca091fc4443
Brad Dwyer <github@barbdwyer.com>
2013-04-13 00:37:37 -0500

    Adding index and webroot

---
+++ webroot/index.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+<title>Phonetroller</title>
+
+<style type="text/css">
+body {
+	text-align: center;
+}
+</style>
+
+</head>
+
+<body>
+
+<br />
+<img src="logo.jpg" id="logo" title="Phonetroller Logo" />
+
+</body>
+
+</html>
