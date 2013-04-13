3ce9fc195b5165033d9f9636fca5b33570b58928
Brad Dwyer <github@barbdwyer.com>
2013-04-13 00:41:09 -0500

    Twitter Link

--- webroot/index.php
+++ webroot/index.php
@@ -7,6 +7,17 @@
 body {
 	text-align: center;
 }
+
+a {
+	color: black;
+	text-decoration: none;
+}
+a:visited {
+	color: black;
+}
+a:hover {
+	text-decoration: underline;
+}
 </style>
 
 </head>
@@ -15,6 +26,8 @@ body {
 
 <br />
 <img src="logo.jpg" id="logo" title="Phonetroller Logo" />
+<br /><br />
+<a href="http://www.twitter.com/phonetroller">twitter</a> | <a href="https://www.facebook.com/pages/Phonetroller/506683629367369">facebook</a>
 
 </body>
 
